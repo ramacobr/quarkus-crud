ffad3cd4ff053117e3ac22a8bc8707d2305168dc
Ramacobr <rafael.correa82@gmail.com>
2025-04-28 21:40:21 -0300

    add methods to conversion

--- src/main/java/org/ramacox/entity/dto/Product.java
+++ src/main/java/org/ramacox/entity/dto/Product.java
@@ -1,6 +1,7 @@
 package org.ramacox.entity.dto;
 
 import lombok.*;
+import org.ramacox.entity.ProductPanache;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,4 +12,19 @@ public class Product {
     public Long id;
     public String description;
     public Double price;
+
+    public static Product fromProductEntity(ProductPanache productPanache) {
+        return Product.builder()
+                .name(productPanache.name)
+                .id(productPanache.id)
+                .description(productPanache.description)
+                .price(productPanache.price).build();
+    }
+
+    public ProductPanache toProductEntity() {
+        return ProductPanache.builder()
+                .name(this.name)
+                .description(this.description)
+                .price(this.price).build();
+    }
 }
